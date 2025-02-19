8410dde80b180fba2fb45140c24bbbc229b07e3d
mohamed mrdoctor <mhamedfekry0@gmail.com>
2025-02-19 03:19:04 +0200

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,21 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "next-themes"; // استيراد ThemeProvider
-
+import localFont from "next/font/local";
+import { ThemeProvider } from "next-themes"; 
+import type { Viewport } from 'next';
+const poppins = localFont({
+  src: "./fonts/Poppins-Black.ttf",
+  variable: "--font-geist-sans",
+  weight: "100 200 300 400 500 600 700 800 900",
+});
+const cairo = localFont({
+  src: "./fonts/Cairo-VariableFont_slnt,wght.ttf",
+  variable: "--font-geist-mono",
+  weight: "100 200 300 400 500 600 700 800 900",
+});
+export const viewport: Viewport = {
+  themeColor: "#6366f1",
+};
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
