39d98fe20c1cf28ad9259b19ca6513913ab68e76
mohamed mrdoctor <mhamedfekry0@gmail.com>
2025-02-19 03:13:52 +0200

    Update

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // تغيير مجلد البنية إلى "build"
-  distDir: 'build',
   experimental: {
     reactCompiler: {
       compilationMode: 'annotation',
