97b5d4583ff8c815cb837b41ac7434398d794aa5
mohamed mrdoctor <mhamedfekry0@gmail.com>
2025-03-05 13:36:52 +0200

    Fix Hero Section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Spotlight } from "@/components/ui/Spotlight";
 import { Globe } from "../ui/Global";
 import { Particles } from "../ui/particles";
