99d4168af11d34da5bdf84f39bbe0f1401fe37aa
mohamed mrdoctor <mhamedfekry0@gmail.com>
2025-03-05 16:14:41 +0200

    Fix Hero Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import About from "@/components/About/About";
 import MainHeadr from "@/components/Header/MainHeadr";
 import Hello from "@/components/Hello/Hello";
 import Hero from "@/components/Hero/Hero";
-import SmoothScrollComponent from "@/components/Test/Test";
+// import SmoothScrollComponent from "@/components/Test/Test";
 import { useState } from "react";
 
 export default function Home() {
